c23330c8c14232632a4627c74a96aad96e62a800
Valery Kiseliou <valery.kiselev@bdtech.by>
2020-02-14 01:10:00 +0300

    Updated base animation.

--- lesson_1.bot/src/scene/map/models/ModelBase.js
+++ lesson_1.bot/src/scene/map/models/ModelBase.js
@@ -1,7 +1,7 @@
 import { BoxGeometry, MeshBasicMaterial } from 'three'
 import Model from './base/Model'
 import ModelOptionsBase from './base/ModelOptionsBase'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import AnimationBase from './animation/AnimationBase'
 
 export default class ModelBase extends Model {
   /**
@@ -9,7 +9,7 @@ export default class ModelBase extends Model {
    * @param {Team} team
    * @param {GLTF} gltf
    */
-  constructor(team) {
+  constructor(team, gltf) {
     super(team, new ModelOptionsBase().setHealth(800))
 
     /**
@@ -24,14 +24,19 @@ export default class ModelBase extends Model {
      */
     this.material = new MeshBasicMaterial({ color: team.color, transparent: true, opacity: 0 })
 
-    const loader = new GLTFLoader()
-    loader.load('/models/base/base.glb', (glb) => {
-      const mesh = glb.scene.children[0]
-      mesh.scale.set(25,25,25)
-      mesh.material.metalness = 0
-      // console.log(mesh)
-      this.add(mesh)
-      this.position.setY(0)
-    })
+    /**
+     *
+     * @type {AnimationBase}
+     */
+    this.animation = new AnimationBase(gltf)
+
+    gltf.model.scale.set(25, 25, 25)
+    gltf.model.material.metalness = 0.2
+    gltf.model.position.setY(1.5)
+    this.add(gltf.model)
+  }
+
+  update(delta) {
+    this.animation.update(delta)
   }
 }
---
+++ lesson_1.bot/src/scene/map/models/animation/AnimationBase.js
@@ -0,0 +1,104 @@
+import { AnimationMixer, AnimationAction } from 'three'
+
+class AnimationBase {
+  /**
+   *
+   * @param {GLTF} gltf
+   */
+  constructor(gltf) {
+    /**
+     *
+     * @type {AnimationMixer}
+     */
+    this.mixer = new AnimationMixer(gltf.model)
+
+    // /**
+    //  *
+    //  * @type {AnimationAction}
+    //  */
+    // this.actionFollowing = this.mixer.clipAction(AnimationBase.findAction(gltf.animations, 'Following'))
+
+    /**
+     *
+     * @type {(AnimationAction|?)}
+     */
+    this.activeAction = null
+  }
+
+  /**
+   *
+   * @param {Array.<AnimationClip>} animations
+   * @param {string} name
+   * @returns {AnimationClip}
+   */
+  static findAction(animations, name) {
+    return animations.find((item) => item.name === name)
+  }
+
+  /**
+   *
+   * @returns {AnimationBase}
+   */
+  pauseAction(action) {
+    action.paused = true
+    return this
+  }
+
+  /**
+   *
+   * @returns {AnimationBase}
+   */
+  clearActiveAction() {
+    if (this.activeAction) {
+      this.pauseAction(this.activeAction)
+      this.activeAction = null
+    }
+    return this
+  }
+
+  /**
+   *
+   * @param {AnimationAction} action
+   * @param {number} [duration]
+   * @returns {AnimationBase}
+   */
+  enableAction(action, duration = 0.4) {
+    if (this.activeAction === action) {
+      return this
+    }
+    if (this.activeAction) {
+      this.activeAction.paused = false
+    }
+    action.paused = false
+    action.enabled = true
+    action.setEffectiveTimeScale(1)
+    action.setEffectiveWeight(1)
+    action.time = 0
+    if (this.activeAction) {
+      this.activeAction.crossFadeTo(action, duration, true)
+    }
+    action.play()
+    this.activeAction = action
+    return this
+  }
+
+  /**
+   *
+   * @returns {AnimationBase}
+   */
+  followingAnimation() {
+    // this.enableAction(this.actionFollowing)
+    return this
+  }
+
+  /**
+   *
+   * @param {number} delta
+   * @returns {void}
+   */
+  update(delta) {
+    this.mixer.update(delta)
+  }
+}
+
+export default AnimationBase
